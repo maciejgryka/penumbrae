c6c7a6ea31736948e1388ee216b65b7d3ed5566f
Maciej Gryka <maciej.gryka@gmail.com>
2011-04-18 13:07:32 +0100

    some better function for cleaning up slices in PenumbraDescriptor

--- ensureProfileRising.m
+++ ensureProfileRising.m
@@ -1,4 +1,4 @@
-function [prof p1 p2] = ensureProfileRising(im, p1, p2)
+function [p1 p2] = ensureProfileRising(im, p1, p2)
    prof = improfile(im, [p1(1) p2(1)] , [p1(2) p2(2)]);
     if prof(1) > prof(size(prof,1))
         prof = flipud(prof);
--- getPenumbraDescriptor.m
+++ getPenumbraDescriptor.m
@@ -25,20 +25,45 @@ function d = getPenumbraDescriptor(im, mask, pixel, n_angles, length)
         slice = slice+1;
         [pixel_offset(1) pixel_offset(2)] = polar2cartesian(hl, d.orientation+ang);
         
-%         d.points(slice, 1, :) = checkImBounds(pixel + pixel_offset, size(mask));
-%         d.points(slice, 2, :) = checkImBounds(pixel - pixel_offset, size(mask));
-
+        % check if the slice is within image
         [d.points(slice, 1, :), ...
-         d.points(slice, 2, :)] = getValidSlice(mask, ...
-                                                    pixel + pixel_offset, ...
-                                                    pixel - pixel_offset);
-                                                                           
-        [d.slices_mask{slice} d.points(slice, 1, :) d.points(slice, 2, :)] = ensureProfileRising(mask, d.points(slice, 1, :), d.points(slice, 2, :));
-        [d.slices_im{slice} d.points(slice, 1, :) d.points(slice, 2, :)] = ensureProfileRising(im, d.points(slice, 1, :), d.points(slice, 2, :));
+         d.points(slice, 2, :)] = getSliceWinthinImage(mask, ...
+                                                       pixel + pixel_offset, ...
+                                                       pixel - pixel_offset);
+        
+        % ensure that the profile is rising (reverse points if it's not)
+        [d.points(slice, 1, :) d.points(slice, 2, :)] = ensureProfileRising(mask, d.points(slice, 1, :), d.points(slice, 2, :));
+        
+        % remove parts outside of penumbra (gradient = 0)
+        [d.points(slice, 1, :), ...
+         d.points(slice, 2, :)] = removeZeroGradient(mask, ...
+                                                     d.points(slice, 1, :), ...
+                                                     d.points(slice, 2, :));
+
+        d.slices_mask{slice} = improfile(mask, d.points(slice, :, 1), d.points(slice, :, 2));
+        d.slices_im{slice} = improfile(im, d.points(slice, :, 1), d.points(slice, :, 2));
     end
 end
 
-function [p1, p2] = getValidSlice(im, p1, p2)
+function [p1, p2] = extendSlice(im, p1, p2)
+% extends the slice to ensure that 0-gradient is reached on both ends
+    [cx, cy, c] = improfile(im, [p1(1) p2(1)], [p1(2) p2(2)]);
+end
+
+function [p1,p2] = removeZeroGradient(im, p1, p2)
+% returns endpoints of the profile, which doesn't include 0-gradient
+% TODO: should probably blur first
+    [cx, cy, c] = improfile(im, [p1(1) p2(1)], [p1(2) p2(2)]);
+    g = gradient(c);
+    fnz = find(g > 0, 1);   % first non-zero gradient
+    lnz = size(g,1) - find(flipud(g) > 0, 1); % last non-zero gradient
+    cx = cx(fnz:lnz);
+    cy = cy(fnz:lnz);
+    p1 = [cx(1) cy(2)];
+    p2 = [cx(size(cx,1)) cy(size(cy,1))];
+end
+
+function [p1, p2] = getSliceWinthinImage(im, p1, p2)
 % returns endpoints of the profile, which lie within the image.
 % p1 and p2 is a pair of end points  of the slice
     % TODO:using improfile here is slow, might want to improve later
