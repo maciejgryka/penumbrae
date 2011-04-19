73ef22a8b4d1b9751eb7c3beaf5feca80de49418
Maciej Gryka <maciej.gryka@gmail.com>
2011-04-19 11:29:34 +0100

    removed a file

--- ensureProfileRising.m
+++
@@ -1,9 +0,0 @@
-function [p1 p2] = ensureProfileRising(im, p1, p2)
-   prof = improfile(im, [p1(1) p2(1)] , [p1(2) p2(2)]);
-    if prof(1) > prof(size(prof,1))
-        prof = flipud(prof);
-        temp = p1;
-        p1 = p2;
-        p2 = temp;
-    end
-end
