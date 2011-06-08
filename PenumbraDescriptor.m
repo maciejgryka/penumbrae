4cb8cd60b29e8f320272e831d3e5dd3e1936dcee
Maciej Gryka <maciej.gryka@gmail.com>
2011-06-08 14:34:36 +0100

    substantial rewrite of PenubraDescriptor to divide each slice into two spokes (negative and positive offset from the center); number of spokes (new) is twice the number of slices (old); renamed .slices_shad to .spokes; changed points to a n_spokes-by-2 matrix of endpoints for spokes - the first point is common (.center)

--- PenumbraDescriptor.m
+++ PenumbraDescriptor.m
@@ -2,10 +2,7 @@ classdef PenumbraDescriptor
     properties
         center
         center_pixel
-%         slices_matte
-%         slices_matte_cat
-        slices_shad
-        slices_shad_cat
+        spokes
         points
     end
     
@@ -15,53 +12,41 @@ classdef PenumbraDescriptor
             if nargin==0
                 d.center = 0;
                 d.center_pixel = 0;
-%                 d.slices_matte = 0;
-                d.slices_shad = 0;
+                d.spokes = 0;
                 d.points = 0;
                 return
             end
 
-            % storage for slices and points
             d.center = pixel;
-            d.points = zeros(n_angles, 2, 2);
-            d.slices_shad = zeros(n_angles, len+1);
-
             if exist('matte', 'var') && ~isempty(matte)
-%                 d.slices_matte = zeros(n_angles, len+1);
-                dsim = matte;
                 d.center_pixel = matte(pixel(2), pixel(1));
             else
-                dsim = shad;
+                d.center_pixel = NaN;
             end
+            % each spoke has two endpoints: d.center and one of d.points
+            % there are two spokes at each angle
+            d.points = zeros(2*n_angles, 2);
+            d.spokes = zeros(n_angles, len+1);
             
             hl = len/2; % half length
 
             ang_step = pi/n_angles;
-            slice_index = 0;
-
+            spoke_index = 1;
             for ang = 0:ang_step:pi-ang_step
-                slice_index = slice_index+1;
                 [pixel_offset(1) pixel_offset(2)] = pol2cart(ang, hl);
                 
-                [d.points(slice_index, 1, :) d.points(slice_index, 2, :)] = ...
-                    processSlice(dsim, pixel - pixel_offset,  pixel + pixel_offset);
-                if isnan(d.points(slice_index, 1, :))
-                    d.points = NaN;
-                    return;
-                end
+                d.points(spoke_index, :) = d.center - pixel_offset;
+                d.points(spoke_index+1, :) = d.center + pixel_offset;
 
-                sl = improfile(shad, d.points(slice_index, :, 1), d.points(slice_index, :, 2));
-                d = d.setSliceShad(slice_index, sl);
-                
-%                 if exist('matte', 'var')
-%                     d = d.setSliceMatte(slice_index, improfile(matte, d.points(slice_index, :, 1), d.points(slice_index, :, 2)));
-%                 end
+                d = d.fillSpoke(d, shad, spoke_index);
+                d = d.fillSpoke(d, shad, spoke_index+1);
+
+                spoke_index = spoke_index + 2;
             end
-            % concatenate the slices into one vector for easy knn lookup
-            d.slices_shad_cat = reshape(d.slices_shad', n_angles*(len+1), 1)';
-%             if exist('matte', 'var')
-%                 d.slices_matte_cat = reshape(d.slices_matte', n_angles*(len+1), 1)';
-%             end
+        end
+        
+        function d = fillSpoke(d, im, sp)
+            d.spokes(sp,:) = improfile(im, d.center, d.points(sp, :));
         end
         
         function d = setSliceShad(d, i, slice)
@@ -69,26 +54,13 @@ classdef PenumbraDescriptor
             if length(slice) > length(d.slices_shad(i, :))+1
                 error('Slice too long, cannot set.');
             end
-            d.slices_shad(i, 1:length(slice)) = gradient(slice);
+            d.spokes(i, 1:length(slice)) = gradient(slice);
         end
-        
-%         function d = setSliceMatte(d, i, slice)
-%             d.slices_matte(i,:) = 0;
-%             if length(slice) > length(d.slices_matte(i, :))+1
-%                 error('Slice too long, cannot set.');
-%             end
-%             d.slices_matte(i, 1:length(slice)) = gradient(slice);
-%         end
         
         function slice = getSliceShad(d, i)
             slice = d.slices_shad(i, :);
             slice = slice(~isnan(slice));
         end
-        
-%         function slice = getSliceMatte(d, i)
-%             slice = d.slices_matte(i, :);
-%             slice = slice(~isnan(slice));
-%         end
         
         function draw(d, plot_color)
             if ~exist('plot_color', 'var')
@@ -100,40 +72,4 @@ classdef PenumbraDescriptor
             plot(d.center(1), d.center(2), 'xr');
         end
     end
-end
-
-function [p1, p2] = processSlice(im, p1, p2)
-    % check if the slice is within image and penumbra boundaries
-    [p1, p2] = getSliceWithinImage(im, p1, p2);
-    if isnan(p1)
-        return;
-    end
-
-    if isempty(p1) || isempty(p2)
-        error('p1 or p2 empty');
-    end
-end
-
-function [p1, p2] = getSliceWithinImage(im, p1, p2)
-% returns endpoints of the profile, which lie within the image.
-% p1 and p2 is a pair of end points  of the slice
-    % TODO: using improfile here is slow (only need cx and cy), might want 
-    % to improve later
-    
-    [cx, cy, c] = improfile(im, [p1(1) p2(1)], [p1(2) p2(2)]);
-    
-    % get coords within image boundary
-    cx_valid = round(cx) >= 1 & round(cx) <= size(im,2);
-    cy_valid = round(cy) >= 1 & round(cy) <= size(im,1);
-
-    % list of valid points
-    vp = round([cx(cx_valid & cy_valid) cy(cx_valid & cy_valid)]);
-    
-    if size(vp,1) == 0 || size(vp,2) == 0
-        p1 = NaN;
-        return;
-    end
-    
-    p1 = double(vp(1,:));
-    p2 = double(vp(size(vp,1),:));
 end
