83bfe9f2020720656a19c82841a86ef1b3d0f1e1
Maciej Gryka <maciej.gryka@gmail.com>
2011-06-16 15:06:58 +0100

    tryToMatch now uses normalization from saveDescriptors and consideres both gradient and absolute intensity

--- tryToMatch.m
+++ tryToMatch.m
@@ -5,7 +5,7 @@ function tryToMatch()
     w = size(matte, 2);
     h = size(matte, 1);
     
-    k = 10;
+    k = 5;
     
     mattes = cell(length(scales));
 
@@ -26,6 +26,8 @@ function tryToMatch()
         penumbra_mask_s = addBorders(penumbra_mask, 1);
         recovered_matte = addBorders(ones(h, w), 1);
         recovered_matte = ones(size(recovered_matte));
+        recovered_mx = zeros(size(recovered_matte));
+        recovered_my = zeros(size(recovered_matte));
         
         % get pixels where descriptors at given sale can be calculated
         penumbra_mask_s = getPenumbraMaskAtScale(penumbra_mask_s, scales(sc));
@@ -41,23 +43,27 @@ function tryToMatch()
 
 %         for n = 1:length(p_pix)
 %             c_descrs(n) = PenumbraDescriptor(shad_s, pixel(n,:), n_angles, len);
-% %             imshow(shad_s);
-% %             hold on;
-% %             c_descrs(n).draw();
-% %             hold off;
 %         end
-%         save(['c_descrs/c_descrs_' suffix '_' int2str(scales(sc)) '.mat'], 'c_descrs');
+%         % current (test) spokes
+%         c_spokes = cat(1,c_descrs(:).spokes);
+%         save(['c_descrs/c_descrs_' suffix '_' int2str(scales(sc)) '.mat'], 'c_descrs', 'c_spokes');
 
         load(['c_descrs/c_descrs_' suffix '_' int2str(scales(sc)) '.mat']);
         
-%         drawDescr(shad_s, c_descrs);
-        [best_descrs dists] = knnsearch(spokes,cat(1,c_descrs(:).spokes),'K', k);
+        fprintf('\tnormalizing...\n');
+        n_spokes = size(c_spokes,1);
+        c_spokes = (c_spokes - repmat(spokes_mu, n_spokes, 1))./repmat(spokes_std, n_spokes, 1);
         
+%         drawDescr(shad_s, c_descrs);
+        fprintf('\tfinding nearest neighbors...\n');
+        [best_descrs dists] = knnsearch(spokes,c_spokes,'K', k);
+        
+        fprintf('\tweighting suggestions...\n');
         % turn spoke indices into descriptor indices
         best_descrs = ceil(best_descrs./(n_angles*2));
         
-        best_descrs = reshape(best_descrs', size(best_descrs,1)/(n_angles*2), n_angles*2*k);
-        dists = reshape(dists', size(dists,1)/(n_angles*2), n_angles*2*k);
+        best_descrs = reshape(best_descrs', n_angles*2*k, size(best_descrs,1)/(n_angles*2))';
+        dists = reshape(dists', n_angles*2*k, size(dists,1)/(n_angles*2))';
         
         % turn descriptor indices into matte values
         best_mattes = cat(1,center_pixels(best_descrs));
@@ -92,16 +98,16 @@ function tryToMatch()
         subplot(2,2,3);
         imshow(shad_s ./ mattes{sc});
         subplot(2,2,4);
-        ms = matte_s .* (mattes{sc} < 1);
+        ms = matte_s .* (penumbra_mask_s == 1);
         ms(ms == 0) = 1;
         imshow(ms);
         
-        figure;
-        subplot(1,3,1);
-        imshow(errim(:,:,1));
-        subplot(1,3,2);
-        imshow(errim(:,:,2));
-        subplot(1,3,3);
-        imshow(errim(:,:,3));
+%         figure;
+%         subplot(1,3,1);
+%         imagesc(errim(:,:,1));
+%         subplot(1,3,2);
+%         imagesc(errim(:,:,2));
+%         subplot(1,3,3);
+%         imagesc(errim(:,:,3));
     end
 end
