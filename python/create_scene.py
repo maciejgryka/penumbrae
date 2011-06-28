6e0488cf89ae6e4a25948615f7408f9ca35748a4
Maciej Gryka <maciej.gryka@gmail.com>
2011-06-28 17:03:59 +0100

    fixed up geometry setup and rendering, added some shaders

--- python/create_scene.py
+++ python/create_scene.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import call
 from pymel.core import *
 
 folder = os.path.dirname(os.path.abspath(__file__))
@@ -11,40 +12,51 @@ if os.path.exists(in_path):
 else:
     f = newFile(f=1)
 
-# setup scene attributes
+# create shaders and shading groups
+texfiles =  os.listdir(os.path.join('/', folder, 'textures'))
 
+shaders_groups = {}
+
+for tex in texfiles:
+    tex_name = tex.split('.')[0]
+    shdr, sg = createSurfaceShader('lambert', 'plain')
+    shaders_groups[tex_name + 'Sh'] = shdr
+    shaders_groups[tex_name + 'SG'] = sg
+
+print shaders_groups.keys()
 
 # setup scene geometry
-if not objExists('renderCam'):
-    renderCam = nt.Camera(name='renderCam')
-    renderCam.getParent().translateY.set(10.0)
-    renderCam.getParent().rotateX.set(-90.0)    
+renderCam = nt.Camera()
+renderCam.getParent().rename('renderCam')
+renderCam.getParent().translateY.set(10.0)
+renderCam.getParent().rotateX.set(-90.0)    
 
-if not objExists('groundPlane'):
-    groundPlane = polyPlane(name='groundPlane')
-    groundPlane[0].scaleX.set(100.0)
-    groundPlane[0].scaleZ.set(100.0) 
+groundPlane = polyPlane(name='groundPlane')
+groundPlane[0].scaleX.set(30.0)
+groundPlane[0].scaleZ.set(30.0)
 
-if not objExists('occluder'):
-    occluder = polyPlane(name='occluder')
-    occluder[0].translateY.set(5.0)
-    occluder[0].translateX.set(-5.0)
-    occluder[0].scaleX.set(10.0)
-    occluder[0].scaleZ.set(100.0)
-    occluder[0].setAttr('primaryVisibility', False)
+occluder = polyPlane(name='occluder')
+occluder[0].translateY.set(5.0)
+occluder[0].translateX.set(-5.0)
+occluder[0].scaleX.set(10.0)
+occluder[0].scaleZ.set(30.0)
+occluder[0].setAttr('primaryVisibility', False)
 
-if not objExists('light1'):
-    light1 = nt.AreaLight(name='light1')
-    light1.getParent().translateY.set(7.5)
-    light1.getParent().rotateX.set(-90.0)
-    light1.setAttr('decayRate', 2)
-    light1.setAttr('intensity', 100)
-    light1.setAttr('useRayTraceShadows', True)
-    light1.setAttr('shadowRays', 100)
-    light1.setAttr('areaLight', True)
-    light1.setAttr('areaType', 0)
-    light1.setAttr('areaHiSamples', 100)
-    connectAttr(light1.instObjGroups[0], SCENE.defaultLightSet.dagSetMembers[0])
+light1 = nt.AreaLight(name='light1')
+light1.getParent().translateY.set(7.5)
+light1.getParent().rotateX.set(-90.0)
+light1.getParent().scaleX.set(0.5);
+light1.getParent().scaleZ.set(0.5);
+light1.setAttr('decayRate', 2)
+light1.setAttr('intensity', 100)
+light1.setAttr('useRayTraceShadows', True)
+light1.setAttr('shadowRays', 100)
+light1.setAttr('areaLight', True)
+light1.setAttr('areaType', 0)
+light1.setAttr('areaHiSamples', 100)
+# make sure the light illuminates everything
+connectAttr(light1.instObjGroups[0], SCENE.defaultLightSet.dagSetMembers[0])
 
-print out_path
 saveAs(out_path, f=1)
+
+call("render -r mr -cam renderCam " + out_path)
