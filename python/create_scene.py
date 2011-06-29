59c75daf066433b8b138b66e8c522dee9b8a9fc6
Maciej Gryka <maciej.gryka@gmail.com>
2011-06-29 10:54:59 +0100

    cleaned up creating tex_files and tex_names lists; also creating shading nodes and putting them into shaders_groups dictionary

--- python/create_scene.py
+++ python/create_scene.py
@@ -2,28 +2,35 @@ import os
 from subprocess import call
 from pymel.core import *
 
-folder = os.path.dirname(os.path.abspath(__file__))
-in_path = os.path.join('/', folder, 'base.mb')
-out_path = os.path.join('/', folder, 'scene.mb')
+from IPython.Debugger import Tracer; debug_here = Tracer()
 
-print in_path
-if os.path.exists(in_path):
-    f = openFile(in_path, f=1)
+current_folder = os.path.dirname(os.path.abspath(__file__))
+base_path = os.path.join('/', current_folder, 'base.mb')
+scene_path = os.path.join('/', current_folder, 'scene.mb')
+
+print base_path
+if os.path.exists(base_path):
+    f = openFile(base_path, f=1)
 else:
     f = newFile(f=1)
 
-# create shaders and shading groups
-texfiles =  os.listdir(os.path.join('/', folder, 'textures'))
-
+# shaders and shading groups
 shaders_groups = {}
 
-for tex in texfiles:
-    tex_name = tex.split('.')[0]
-    shdr, sg = createSurfaceShader('lambert', 'plain')
-    shaders_groups[tex_name + 'Sh'] = shdr
-    shaders_groups[tex_name + 'SG'] = sg
+# get absolute paths to texture files
+tex_folder = os.path.join('/', current_folder, 'tex')
+tex_files = [os.path.join('/', tex_folder, tex_file) 
+             for tex_file in os.listdir(tex_folder)]
 
-print shaders_groups.keys()
+# get texture names
+tex_names = [tf.split('\\')[-1].replace('.', '_').lower() for tf in tex_files]
+
+for tex in zip(tex_names, tex_files):
+    shader, shading_group = createSurfaceShader('lambert', tex[0])
+    shaders_groups[tex[0] + 'Sh'] = shader
+    shaders_groups[tex[0] + 'SG'] = shading_group
+    shaders_groups[tex[0] + 'SN'] = shadingNode('file', at=True, name=tex[0])
+    shaders_groups[tex[0] + 'SN'].setAttr('fileTextureName', tex[1])
 
 # setup scene geometry
 renderCam = nt.Camera()
@@ -57,6 +64,9 @@ light1.setAttr('areaHiSamples', 100)
 # make sure the light illuminates everything
 connectAttr(light1.instObjGroups[0], SCENE.defaultLightSet.dagSetMembers[0])
 
-saveAs(out_path, f=1)
+saveAs(scene_path, f=1)
 
-call("render -r mr -cam renderCam " + out_path)
+for tex_name in tex_names:
+    sets(shaders_groups[tex_name+'SG'], forceElement=shaders_groups[tex_name+'Sh'])
+    out_im = os.path.join('/', current_folder, tex_name)
+    call("render -r mr -cam renderCam -im %s %s" %(out_im, scene_path))
